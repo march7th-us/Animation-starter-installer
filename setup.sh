26e7e46cf31d96418438c92664ba09adf6f0ea19
cz <88184266+cz050819@users.noreply.github.com>
2024-09-26 21:30:51 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# ID_LIKE=`cat /etc/os-release | grep ID_LIKE | awk -F "=" '{print $2}'`
-# if echo "$ID_LIKE" | grep -qwi "arch";then
-# 	sudo pacman -Syyu
-# 	sudo pacman -S wine wine-mono wine-gecko mingw-w64 git meson nasm glslang
-# elif echo "$ID_LIKE" | grep -qwi "debian";then
-# 	sudo apt-get update
-# 	sudo apt-get wine wine-mono wine-gecko mingw-w64 git meson nasm glslang
-# fi
-
 sudo cp `pwd`/animation-starter
  /usr/bin
 d_path=`pwd`
